a4d70f5ec1de1526d59da53e353851e2c70089f3
Jasper0077 <jasperhwong@hotmail.com>
2023-07-03 23:43:15 +0800

    implement searchbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Image from "next/image";
+import Searchbar from "./Searchbar";
 
-function Header() {
-  return <div>Header</div>;
+function classNames(...classes: Array<string>) {
+  return classes.filter(Boolean).join(" ");
 }
 
-export default Header;
+export default function Header() {
+  return (
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex flex-col sm:flex-row max-w-full items-center justify-between p-6 lg:px-8 bg-gray-500/10"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Trello Clone</span>
+            <Image
+              className="h-8 w-auto"
+              width={300}
+              height={100}
+              src="https://links.papareact.com/c2cdd5"
+              alt="Trello Logo"
+            />
+          </a>
+        </div>
+        <div>
+          {/* Search Box */}
+          <Searchbar />
+        </div>
+      </nav>
+    </header>
+  );
+}
---
+++ components/Searchbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Hotkeys from "react-hot-keys";
+
+interface Props {}
+
+const Searchbar: React.FC<Props> = () => {
+  const ref = React.useRef<HTMLInputElement>(null);
+  return (
+    <Hotkeys
+      keyName="cmd+k"
+      onKeyUp={(_, event, handle) => {
+        ref.current?.focus();
+      }}
+    >
+      <form>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <MagnifyingGlassIcon
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </div>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            ref={ref}
+            className="block bg-gray-200 w-full rounded-2xl border-0 py-1.5 pl-10 text-gray-900 ring-0 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            placeholder="Search"
+          />
+          <span className="absolute inset-y-0 right-0 pr-3 text-sm font-bold flex items-center text-gray-400">
+            ⌘ K
+          </span>
+        </div>
+        <button type="submit" className="hidden">
+          Submit
+        </button>
+      </form>
+    </Hotkeys>
+  );
+};
+
+export default Searchbar;
