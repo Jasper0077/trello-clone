08fa3e90b97174c621cc9355b296dd4284b33aae
Jasper0077 <jasperhwong@hotmail.com>
2023-07-03 23:43:04 +0800

    add Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
       {/* Header */}
+      <Header />
       {/* Board */}
       <h1>Trello Clone</h1>
     </main>
-  )
+  );
 }
