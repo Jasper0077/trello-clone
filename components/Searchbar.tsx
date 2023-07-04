025610783879108dec91119039db0e1d26e503b4
Jasper0077 <jasperhwong@hotmail.com>
2023-07-04 21:45:59 +0800

    minor fix on searchbar border

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -16,7 +16,7 @@ const Searchbar: React.FC<Props> = () => {
       }}
     >
       <form>
-        <div className="relative mt-2 rounded-md shadow-sm">
+        <div className="relative mt-2 rounded-2xl shadow-sm">
           <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
             <MagnifyingGlassIcon
               className="h-5 w-5 text-gray-400"
