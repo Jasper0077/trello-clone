5ef25260451035278bd8ef7b787c6425d4787d48
Jasper0077 <jasperhwong@hotmail.com>
2023-07-04 21:45:44 +0800

    setup header layout and styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Trello 2.0',
-  description: 'AI powered Trello Clone',
-}
+  title: "Trello 2.0",
+  description: "AI powered Trello Clone"
+};
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="bg-[#F5F6F8]">{children}</body>
     </html>
-  )
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import Searchbar from "./Searchbar";
+import Avatar from "react-avatar";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
 
 function classNames(...classes: Array<string>) {
   return classes.filter(Boolean).join(" ");
@@ -7,11 +11,28 @@ function classNames(...classes: Array<string>) {
 
 export default function Header() {
   return (
-    <header className="bg-white">
+    <header>
       <nav
-        className="mx-auto flex flex-col sm:flex-row max-w-full items-center justify-between p-6 lg:px-8 bg-gray-500/10"
+        className="mx-auto flex flex-col sm:flex-row max-w-full items-center p-6 lg:px-8 bg-gray-500/10"
         aria-label="Global"
       >
+        <div
+          className="
+          absolute
+          top-0
+          left-0
+          w-full
+          h-96
+          bg-gradient-to-br
+          from-pink-400
+          to-[#0055D1]
+          rounded-md
+          filter
+          blur-3xl
+          opacity-50
+          -z-50
+        "
+        />
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Trello Clone</span>
@@ -24,11 +45,23 @@ export default function Header() {
             />
           </a>
         </div>
-        <div>
-          {/* Search Box */}
+        <div className="flex flex-row items-end justify-between gap-4">
           <Searchbar />
+          <Avatar
+            className=""
+            name="Jasper Hwong"
+            round
+            size="36"
+            color="#0055D1"
+          />
         </div>
       </nav>
+      <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center text-sm font-light pr-5 shadow-xl rounded-2xl w-fit bg-white max-w-3xl text-[#0055D1]">
+          <UserCircleIcon className="inline-block h-10 w-10 tex-[#0055D1] mr-1" />
+          GPT is summarising your task for the day...
+        </p>
+      </div>
     </header>
   );
 }
