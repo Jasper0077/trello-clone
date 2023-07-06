6763e5bbd7476bd21d3e54c2f0e698b6563f877d
Jasper0077 <jasperhwong@hotmail.com>
2023-07-06 23:00:49 +0800

    add drag drop wrapper

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { useBoardStore } from "@/store/BoardStore";
 import React from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
+import Column from "./Column";
 
 function Board() {
   const [board, getBoard] = useBoardStore((state) => [
@@ -12,14 +15,25 @@ function Board() {
     getBoard();
   }, [getBoard]);
 
+  const handleOnDragEnd = (reuslt: DropResult) => {};
+
   console.log(board);
   return (
-    <h1>Board</h1>
-    // <DragDropContext>
-    //   <Droppable droppableId="board" direction="horizontal" type="column">
-    //     {(provided) => <div></div>}
-    //   </Droppable>
-    // </DragDropContext>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) => (
+          <div
+            className="grid grid-cols-1 md:grid-cols-4 gap-5 max-w-7xl mx-auto"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {Array.from(board.columns.entries()).map(([id, column], index) => (
+              <Column key={id} id={id} tasks={column.tasks} index={index} />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
 
