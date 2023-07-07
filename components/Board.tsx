a9604f1d47136109c07f3dadefcdd0b78424bb9a
Jasper0077 <jasperhwong@hotmail.com>
2023-07-08 01:17:45 +0800

    update tasks to save in db

--- components/Board.tsx
+++ components/Board.tsx
@@ -4,18 +4,93 @@ import { useBoardStore } from "@/store/BoardStore";
 import React from "react";
 import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import Column from "./Column";
+import { Column as ColumnType } from "@/typing";
 
 function Board() {
-  const [board, getBoard] = useBoardStore((state) => [
-    state.board,
-    state.getBoard
-  ]);
+  const [board, getBoard, setBoardState, updateTaskInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTasksInDB
+    ]
+  );
 
   React.useEffect(() => {
     getBoard();
   }, [getBoard]);
 
-  const handleOnDragEnd = (reuslt: DropResult) => {};
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+    console.log(destination, source, type);
+    if (!destination) return;
+
+    // Handle column drag
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns
+      });
+    }
+
+    // Handle task card drag
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: ColumnType = {
+      id: startColIndex[0],
+      tasks: startColIndex[1].tasks
+    };
+
+    const finishCol: ColumnType = {
+      id: finishColIndex[0],
+      tasks: finishColIndex[1].tasks
+    };
+
+    if (!startCol || !finishCol) return;
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTasks = startCol.tasks;
+    const [taskMoved] = newTasks.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      newTasks.splice(destination.index, 0, taskMoved);
+      const newCol = {
+        id: startCol.id,
+        tasks: newTasks
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      // drag to another column
+      const finishTasks = Array.from(finishCol.tasks);
+      finishTasks.splice(destination.index, 0, taskMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        tasks: newTasks
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        tasks: finishTasks
+      });
+
+      updateTaskInDB(taskMoved, finishCol.id);
+
+      setBoardState({ ...board, columns: newColumns });
+    }
+  };
 
   console.log(board);
   return (
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,10 +1,13 @@
-import { Board, Column, TypedColumn } from "@/typing";
+import { databases } from "@/appwrite";
+import { Board, Column, Todo, TypedColumn } from "@/typing";
 import { getTasksGroupedByColumn } from "@/utils/getTasksGroupedByColumn";
 import { create } from "zustand";
 
 interface BoardState {
     board: Board;
     getBoard: () => void;
+    setBoardState: (board: Board) => void;
+    updateTasksInDB: (task: Todo, columnID: TypedColumn) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
@@ -14,5 +17,17 @@ export const useBoardStore = create<BoardState>((set) => ({
     getBoard: async() => {
         const board = await getTasksGroupedByColumn();
         set({ board });
+    },
+    setBoardState: (board) => set({ board }),
+    updateTasksInDB: async (task: Todo, columnId: TypedColumn) => {
+        await databases.updateDocument(
+            process.env.NEXT_PUBLIC_DATABASE_ID!,
+            process.env.NEXT_PUBLIC_TASK_COLLECTION_ID!,
+            task.$id,
+            {
+                title: task.title,
+                status: columnId
+            }
+        )
     }
 }))
